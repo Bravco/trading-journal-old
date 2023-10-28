d4b6394a47c38da74a89905c0b6bdc3eb59b5810
Bravco <boss.wiw@gmail.com>
2023-10-28 14:45:31 +0200

    Added basic layout and css root vars

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <h1>
+                    <NuxtLink to="/">Trading Journal</NuxtLink>
+                </h1>
+            </nav>
+        </header>
+        <main>
+            <slot/>
+        </main>
+    </div>
+</template>
+
+<style scoped>
+    nav {
+        padding: 1rem 5%;
+    }
+</style>
