fda7f35885ffa1f7d9672bc05dcdb437aad9f034
Bravco <boss.wiw@gmail.com>
2023-10-30 13:54:52 +0100

    Started creating summary cards

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     "nuxt-icon",
+    "@nuxt/ui",
   ],
   css: [
     "~/assets/main.css",
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,80 @@
 <template>
     <div>
-        
+        <div class="summary">
+            <div class="container summary-card">
+                <div class="summary-card-txt">
+                    <span class="summary-card-title">
+                        Profile Value
+                        <UTooltip text="The current total value of your portfolio.">
+                            <Icon name="ph:info-duotone"/>
+                        </UTooltip>
+                    </span>
+                    <span class="summary-card-value">€ 1458.16</span>
+                </div>
+            </div>
+            <div class="container summary-card">
+                <div class="summary-card-txt">
+                    <span class="summary-card-title">
+                        Trade Expectancy
+                        <UTooltip text="">
+                            <Icon name="ph:info-duotone"/>
+                        </UTooltip>
+                    </span>
+                    <span class="summary-card-value">€ +17.84</span>
+                </div>
+            </div>
+            <div class="container summary-card">
+                <div class="summary-card-txt">
+                    <span class="summary-card-title">
+                        Win Rate
+                        <UTooltip text="">
+                            <Icon name="ph:info-duotone"/>
+                        </UTooltip>
+                    </span>
+                    <span class="summary-card-value">% 25.00</span>
+                </div>
+            </div>
+            <div class="container summary-card">
+                <div class="summary-card-txt">
+                    <span class="summary-card-title">
+                        Profit Factor
+                        <UTooltip text="">
+                            <Icon name="ph:info-duotone"/>
+                        </UTooltip>
+                    </span>
+                    <span class="summary-card-value">0.43</span>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+
+<style scoped>
+    .summary {
+        display: flex;
+        gap: 2rem;
+    }
+
+    .summary-card {
+        width: 100%;
+        padding: 2rem;
+    }
+
+    .summary-card-txt {
+        display: flex;
+        flex-direction: column;
+        gap: .5rem;
+    }
+
+    .summary-card-title {
+        display: flex;
+        align-items: center;
+        gap: .5rem;
+        color: var(--color-text-alt);
+    }
+
+    .summary-card-value {
+        font-size: 1.5rem;
+        font-weight: bold;
+    }
+</style>
