68662321ffc512e2dee53f8c81dac077ce9e796f
Bravco <boss.wiw@gmail.com>
2023-10-31 11:07:58 +0100

    Added summary card button style

--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,9 @@
                     </span>
                     <span class="summary-card-value">€ 1458.16</span>
                 </div>
+                <button class="summary-card-btn">
+                    <Icon name="ph:bank-duotone"/>
+                </button>
             </div>
             <div class="container summary-card">
                 <div class="summary-card-txt">
@@ -22,6 +25,9 @@
                     </span>
                     <span class="summary-card-value">€ +17.84</span>
                 </div>
+                <button class="summary-card-btn">
+                    <Icon name="ph:chart-line-duotone"/>
+                </button>
             </div>
             <div class="container summary-card">
                 <div class="summary-card-txt">
@@ -57,6 +63,9 @@
 
     .summary-card {
         width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
         padding: 2rem;
     }
 
@@ -76,5 +85,16 @@
     .summary-card-value {
         font-size: 1.5rem;
         font-weight: bold;
+    }
+    
+    .summary-card-btn {
+        width: 3rem;
+        height: 3rem;
+        display: grid;
+        place-items: center;
+        background-color: var(--color-bg-tertiary);
+        color: var(--color-primary);
+        font-size: 1.5rem;
+        border-radius: .5rem;
     }
 </style>
