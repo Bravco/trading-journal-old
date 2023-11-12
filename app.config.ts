ee09d58f04eea7df1681b31e00655227af70cfe7
Bravco <boss.wiw@gmail.com>
2023-11-12 19:47:39 +0100

    Fixed a css bug

--- app.config.ts
+++ app.config.ts
@@ -6,6 +6,7 @@ export default defineAppConfig({
         dropdown: { background: "bg-gray-800" },
         card: { background: "bg-gray-900" },
         input: { default: { color: "gray" } },
+        select: { default: { color: "gray" } },
         selectMenu: { background: "bg-gray-800" },
         modal: { background: "bg-gray-900" },
     },
